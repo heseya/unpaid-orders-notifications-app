985a2d26ee524e1210d9c92346fc99b26962520c
Jędrzej Buliński <git@jedrula.pl>
2022-08-30 22:23:57 +0200

    style fix

--- app/Services/Contracts/ProductsServiceContract.php
+++ app/Services/Contracts/ProductsServiceContract.php
@@ -3,9 +3,6 @@
 namespace App\Services\Contracts;
 
 use App\Dtos\ProductsExportDto;
-use App\Models\Api;
-use Illuminate\Support\Collection;
-use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 
 interface ProductsServiceContract
