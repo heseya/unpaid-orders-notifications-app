273f08523b184a51bc6ba12147558ed7b562f660
Jędrzej Buliński <git@jedrula.pl>
2022-08-31 23:30:03 +0200

    Test command

--- app/Console/Commands/RefreshProductsFeed.php
+++ app/Console/Commands/RefreshProductsFeed.php
@@ -2,14 +2,9 @@
 
 namespace App\Console\Commands;
 
-use App\Dtos\ProductsExportDto;
-use App\Exceptions\SettingNotFoundException;
 use App\Models\Api;
-use App\Services\Contracts\ProductsServiceContract;
-use Exception;
+use App\Services\Contracts\ApiServiceContract;
 use Illuminate\Console\Command;
-use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\Config;
 
 class RefreshProductsFeed extends Command
 {
@@ -33,7 +28,7 @@ class RefreshProductsFeed extends Command
      * @return void
      */
     public function __construct(
-        private ProductsServiceContract $productsService,
+        private ApiServiceContract $apiService,
     ){
         parent::__construct();
     }
@@ -43,30 +38,88 @@ class RefreshProductsFeed extends Command
      */
     public function handle(): int
     {
-        $apis = Api::all()->each(function ($api) {
-            $url = $api->url;
-            $this->info("[$url] Processing");
+        $apis = Api::all()->each(fn (Api $api) => $this->processApi($api))->count();
 
-            $dto = new ProductsExportDto(
-                api: $api->url,
-                format: Config::get('export.default_format'),
-                params: Collection::make(),
-            );
-
-            try {
-                $this->productsService->reloadProducts($api, $dto, true);
-                $this->productsService->reloadProducts($api, $dto, false);
-            } catch (Exception $exception) {
-                if ($exception instanceof SettingNotFoundException) {
-                    $this->info("[$url] Skipping");
-                } else {
-                    $this->error("[$url] " . $exception->getMessage());
-                }
-            }
-        })->count();
-
-        $this->info("Processed $apis stores");
+        $this->info("Processed $apis apis");
 
         return 1;
+    }
+
+    private function processApi(Api $api): void
+    {
+        $url = $api->url;
+        $path = $this->filePath($api);
+        $this->info("[$url] Processing");
+
+        // create / overwrite file
+        $file = fopen($path, 'w');
+        fwrite($file, $this->headers());
+        fclose($file);
+
+        $lastPage = 1; // Get at least once
+        for ($page = 1; $page <= $lastPage; $page++) {
+            $fullUrl = "/products?full&limit=200&page=${page}";
+            $this->info("Getting page ${page} of {$lastPage}");
+
+            $response = $this->apiService->get($api, $fullUrl);
+            $lastPage = $response->json('meta.last_page');
+
+            // append data
+            $file = fopen($path, 'a');
+
+            foreach ($response->json('data') as $product) {
+                if ($product['cover'] === null || $product['description_short'] === null) {
+                    continue;
+                }
+
+                fwrite($file, $this->product(
+                    $product,
+                    'https://api.***REMOVED***.pl/products',
+                    'Ksiazki.pl',
+                    $response->json('meta.currency.symbol'),
+                ));
+            }
+
+            fclose($file);
+        }
+    }
+
+    private function filePath(Api $api): string
+    {
+        return $api->getKey() . '/products.csv';
+    }
+
+    private function headers(): string
+    {
+        return implode(',', [
+            'id',
+            'title',
+            'description',
+            'availability',
+            'condition',
+            'price',
+            'link',
+            'image_link',
+            'additional_image_link',
+            'brand',
+            'google_product_category',
+        ]) . "\n";
+    }
+
+    private function product(array $product, string $storeFrontUrl, string $storeName, string $currency): string
+    {
+        return implode(',', [
+            $product['id'],
+            $product['name'],
+            $product['description_short'],
+            $product['available'] ? 'in stock' : 'out of stock',
+            'new',
+            "{$product['price']} {$currency}",
+            "$storeFrontUrl/{$product['slug']}",
+            $product['cover'] ? $product['cover']['url'] : '',
+            $product['gallery'][1] ? $product['gallery'][1]['url'] : '',
+            "$storeName",
+            $product['google_product_category'],
+        ]) . "\n";
     }
 }
