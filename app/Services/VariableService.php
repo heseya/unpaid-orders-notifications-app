09507889a17689a187e88cebdfa837e1fcd8b6e0
Jędrzej Buliński <git@jedrula.pl>
2023-04-13 11:10:50 +0200

    Shipping price resolver

--- app/Services/VariableService.php
+++ app/Services/VariableService.php
@@ -12,19 +12,24 @@ use App\Resolvers\GlobalResolver;
 use App\Resolvers\LocalResolver;
 use App\Resolvers\PriceResolver;
 use App\Resolvers\SalePriceResolver;
+use App\Resolvers\ShippingPriceResolver;
 use App\Services\Contracts\VariableServiceContract;
 use Illuminate\Support\Str;
 
 class VariableService implements VariableServiceContract
 {
     private const RESOLVERS = [
+        // Global
+        '$shipping_price' => ShippingPriceResolver::class,
+
+        // Local
         '#cover' => CoverResolver::class,
         '#additional_image' => AdditionalImageResolver::class,
         '#availability' => AvailabilityResolver::class,
         '#price' => PriceResolver::class,
         '#sale_price' => SalePriceResolver::class,
-//        '#product_url' => ProductUrlResolver::class,
         '#ean' => EanResolver::class,
+//        '#product_url' => ProductUrlResolver::class,
     ];
 
     public function resolve(array $keys): array
