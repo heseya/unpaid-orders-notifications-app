e93503deb1d95081d31cbb3153580e4565360a78
Jędrzej Buliński <git@jedrula.pl>
2023-04-18 08:47:07 +0200

    CR fixes

--- front/enum/Permission.ts
+++
@@ -1,9 +0,0 @@
-export enum Permission {
-  ReviewsAdd = 'reviews.add',
-  ReviewsEdit = 'reviews.edit',
-  ReviewsMedia = 'reviews.media',
-  ReviewsRemove = 'reviews.remove',
-
-  ProductsShow = 'products.show',
-  ProductsShowDetails = 'products.show_details',
-}
--- front/views/Feed.vue
+++ front/views/Feed.vue
@@ -6,7 +6,7 @@
   <div v-if="!isLoading">
     <a-page-header
       :title="feed.name"
-      :sub-title="feed.refreshed_at ? `last refreshed: ` + feed.refreshed_at : 'not generated yet'"
+      :sub-title="feed.refreshed_at ? `last refreshed: ${feed.refreshed_at}` : 'not generated yet'"
       @back="() => $router.push({ name: 'Index' })"
     >
       <template #extra>
