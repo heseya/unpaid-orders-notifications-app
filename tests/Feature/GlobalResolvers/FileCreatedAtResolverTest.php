044f731fa866f3721355b4d507a701cb698749bb
Jędrzej Buliński <git@jedrula.pl>
2023-08-11 11:15:50 +0200

    fixes

--- tests/Feature/GlobalResolvers/FileCreatedAtResolverTest.php
+++ tests/Feature/GlobalResolvers/FileCreatedAtResolverTest.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 use App\Resolvers\FileCreatedAtResolver;
 
 it('resolves field', function () {
-    $this->travelTo('2020-01-01T10:10:00+01:00');
+    $this->travelTo('2020-01-01T09:10:00+00:00');
 
     expect(FileCreatedAtResolver::resolve(mockField(new FileCreatedAtResolver())))
-        ->toBe('2020-01-01T10:10:00+01:00');
+        ->toBe('2020-01-01T09:10:00+00:00');
 });
