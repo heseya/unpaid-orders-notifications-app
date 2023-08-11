dc1ba08f265665791a2332eaaad997fd971be9fb
Jędrzej Buliński <git@jedrula.pl>
2023-08-11 11:13:23 +0200

    fixes

--- tests/Feature/GlobalResolvers/FileCreatedAtResolverTest.php
+++ tests/Feature/GlobalResolvers/FileCreatedAtResolverTest.php
@@ -2,12 +2,11 @@
 
 declare(strict_types=1);
 
-use App\Resolvers\ShippingPriceResolver;
-use Illuminate\Support\Facades\Http;
+use App\Resolvers\FileCreatedAtResolver;
 
 it('resolves field', function () {
     $this->travelTo('2020-01-01T10:10:00+01:00');
 
-    expect(ShippingPriceResolver::resolve(mockField(new ShippingPriceResolver())))
+    expect(FileCreatedAtResolver::resolve(mockField(new FileCreatedAtResolver())))
         ->toBe('2020-01-01T10:10:00+01:00');
 });
